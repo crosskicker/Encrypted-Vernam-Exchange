ec58ee1862bf5396d201e626b186320e291cd4cb
crosskicker <121888262+crosskicker@users.noreply.github.com>
2024-09-27 10:28:05 +0200

    Delete test.py

--- test.py
+++
@@ -1,7 +0,0 @@
-
-
-try:
-    from cryptage.crypt import echange_cle_client, decode_str, code_str, echange_cle_serveur
-    print("Import successful!")
-except ImportError as e:
-    print(f"ImportError: {e}")
